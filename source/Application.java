0905b44dbb190175b4df3ff67bdc18bb17fdaced
Nikita <athlon64x2600@gmail.com>
2023-05-01 10:21:16 +0200

    Задание 1. Получить и вывести в консоль полные данные об одном из работников(имя, фамилия, пол, город) по id.

--- source/Application.java
+++ source/Application.java
@@ -1,7 +1,31 @@
 package source;
 
+import java.sql.*;
+
+
 public class Application {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws SQLException {
+        final String user = "postgres";
+        final String pass = "root";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        try (final Connection connection = DriverManager.getConnection(url, user, pass);
+             PreparedStatement statement = connection.prepareStatement("" +
+                     "SELECT  * FROM employee WHERE id = (?)")) {
+            statement.setInt(1, 2);
+            final ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String name = "Name: " + resultSet.getString("first_name");
+                String surname = "Surname: " + resultSet.getString("last_name");
+                String gender = "Gender: " + resultSet.getString(4);
+                String city = "City id: " + resultSet.getInt(6);
+
+                System.out.println(name);
+                System.out.println(surname);
+                System.out.println(gender);
+                System.out.println(city);
+            }
+        }
     }
 }
